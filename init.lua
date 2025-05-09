15e14d2ab93a25a80714c6abe463d92bf63bd379
claudiogoncalves <claudiogoncalves@handtalk.me>
2025-05-09 16:43:14 -0300

    new[remove unused]

--- init.lua
+++ init.lua
@@ -141,10 +141,6 @@ vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
-vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>', { desc = 'NeoTree reveal', silent = true })
-
-
-
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
